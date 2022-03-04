1a1125e6ff523fb4bbb7d7efa5e2f610d68d7d24
Sara van Erp <sara.vanerp@gmail.com>
2022-03-04 09:41:09 +0100

    moved functions to app.R

--- R/app.R
+++ R/app.R
@@ -8,7 +8,139 @@ library(tidySEM)
 library(plotly)
 library(DT)
 
-source("./R/functions_shinyBSEM.R")
+##### Functions -----
+## Function to automatically add intuitive labels to the lavaan syntax
+label_syntax_fun <- function(model, meanstructure = FALSE){
+  parTab <- lavaanify(model, meanstructure = meanstructure, int.ov.free = TRUE, int.lv.free = FALSE,
+                      auto.fix.first = TRUE, auto.fix.single = TRUE, auto.var = TRUE,
+                      auto.cov.lv.x = TRUE, auto.efa = TRUE, auto.th = TRUE,
+                      auto.delta = TRUE, auto.cov.y = TRUE)
+  
+  load <- parTab[parTab$op == "=~", ]
+  if(nrow(load) > 0){
+    load[, "label"] <- paste0("l", 1:nrow(load))
+  }
+  
+  vars <- parTab[parTab$op == "~~" & parTab$lhs == parTab$rhs, ]
+  if(nrow(vars) > 0){
+    vars[, "label"] <- paste0("v", 1:nrow(vars))
+  }
+  
+  corr <- parTab[parTab$op == "~~" & parTab$lhs != parTab$rhs, ]
+  if(nrow(corr) > 0){
+    corr[, "label"] <- paste0("r", 1:nrow(corr))
+  }
+  
+  regr <- parTab[parTab$op == "~", ]
+  if(nrow(regr) > 0){
+    regr[, "label"] <- paste0("b", 1:nrow(regr))
+  }
+  
+  if(meanstructure == TRUE){
+    int <- parTab[parTab$op == "~1", ]
+    if(nrow(int) > 0){
+      int[, "label"] <- paste0("i", 1:nrow(int))
+    }
+    
+    parTabLab <- rbind.data.frame(load, vars, corr, regr, int)
+  } else{
+    parTabLab <- rbind.data.frame(load, vars, corr, regr)
+  }
+  
+  return(parTabLab)
+}
 
+## Function to plot the model using tidySEM
+plot_fun <- function(fit, layout = "layout_as_tree", est = TRUE){
+  prep <- prepare_graph(fit, layout_algorithm = layout)
+  
+  prep$edges$geom_text <- TRUE # to be able to record and use the labels
+  prep$nodes$geom_text <- TRUE # to be able to record and use the labels
+  
+  # show the labels instead of the classical estimates if est == FALSE
+  # TODO: avoid estimating the classical model at all
+  if(est == FALSE){prep$edges$label <- prep$edges$lavaan_label}
+  # show the Bayesian estimates without significance if est == TRUE
+  if(est == TRUE){
+    lb <- prep$edges$label
+    lb1 <- gsub("***", "", lb, fixed = TRUE)
+    lb2 <- gsub("**", "", lb1, fixed = TRUE)
+    lb3 <- gsub("*", "", lb2, fixed = TRUE)
+    prep$edges$label <- lb3
+  }
+  pmod <- plot(prep)
+  return(pmod)
+}
+
+## Funtion to initialize a data frame to store the priors in
+init_df <- function(pars){
+  if(grepl("l", pars) == TRUE){
+    cbind.data.frame(Parameter = pars, Prior = "Normal", 'Hyperparameter 1' = NA, 'Hyperparameter 2' = NA)
+  } else if(grepl("v", pars) == TRUE){
+    cbind.data.frame(Parameter = pars, Prior = "Gamma", 'Hyperparameter 1' = NA, 'Hyperparameter 2' = NA)
+  } else if(grepl("r", pars) == TRUE){
+    cbind.data.frame(Parameter = pars, Prior = "Beta", 'Hyperparameter 1' = NA, 'Hyperparameter 2' = NA)
+  } else if(grepl("b", pars) == TRUE){
+    cbind.data.frame(Parameter = pars, Prior = "Normal", 'Hyperparameter 1' = NA, 'Hyperparameter 2' = NA)
+  }
+}
+
+## Function to set all priors to the blavaan default
+def_prior <- function(df){
+  load <- grep("l", df$pars)
+  df[load, "Hyperparameter 1"] <- 0
+  df[load, "Hyperparameter 2"] <- 10
+  
+  vars <- grep("v", df$pars)
+  df[vars, "Hyperparameter 1"] <- 1
+  df[vars, "Hyperparameter 2"] <- .5
+  
+  cors <- grep("r", df$pars)
+  df[cors, "Hyperparameter 1"] <- 1
+  df[cors, "Hyperparameter 2"] <- 1
+  
+  regs <- grep("b", df$pars)
+  df[regs, "Hyperparameter 1"] <- 0
+  df[regs, "Hyperparameter 2"] <- 10
+  
+  return(df)
+}
+
+## Function to add priors in correct form to the prior dataframe
+priorspec <- function(df){
+  if(df["Prior"] == "Normal"){
+    spec <- paste0("prior(\"dnorm(", df["Hyperparameter 1"], ", ", df["Hyperparameter 2"], ")\")* ")
+  }
+  if(df["Prior"] == "Gamma"){
+    spec <- paste0("prior(\"dgamma(", df["Hyperparameter 1"], ", ", df["Hyperparameter 2"], ")\")* ")
+  }
+  if(df["Prior"] == "Beta"){
+    spec <- paste0("prior(\"dbeta(", df["Hyperparameter 1"], ", ", df["Hyperparameter 2"], ")\")* ")
+  }
+  return(spec)
+}
+
+## Function to combine convergence diagnostics
+# Use specific cutoffs for convergence; these are hardcoded only here
+convfun <- function(fit, lbls, totalN, rhatC = 1.1, neffC = 100, neff_ratioC = 0.1){
+  rhat <- blavInspect(fit, what = "psrf")
+  neff <- blavInspect(fit, what = "neff")
+  neff_ratio <- neff/totalN
+  convdf <- cbind.data.frame("Potential scale reduction statistic (Rhat)" = rhat, 
+                             "Effective N"= neff, 
+                             "Ratio effective to total N" = neff_ratio)
+  convdf$`Parameter` <- rownames(convdf)
+  # add original labels
+  lbls$`Parameter` <- paste(lbls$lhs, lbls$op, lbls$rhs, sep = "")
+  lbls.sel <- lbls[, c("Parameter", "label")]
+  colnames(lbls.sel) <- c("Parameter", "Label")
+  df <- merge(lbls.sel, convdf, by = "Parameter")
+  conv <- if(sum(rhat > rhatC, na.rm = TRUE) == 0 && sum(neff < neffC, na.rm = TRUE) == 0 && sum(neff_ratio < neff_ratioC, na.rm = TRUE) == 0){
+    "yes"
+  } else{ "no" }
+  return(list(df = df, rhatC = rhatC, neffC = neffC, neff_ratioC = neff_ratioC, conv = conv))
+}
+
+##### Run the app -----
 runApp("./R")
  
